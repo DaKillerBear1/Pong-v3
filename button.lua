b4926856e30fa2309548e31d829f296d60045742
Björn <bskouborg@gmail.com>
2016-10-27 21:24:40 +0200

    Continued work on buttons

--- button.lua
+++ button.lua
@@ -5,7 +5,7 @@ Button = {
   y = 0,
   width = 0,
   height = 0,
-  colour = {255, 255, 255},
+  colour = {255, 140, 0, 255},
   
   new = function(self)
     t = {}
@@ -19,19 +19,39 @@ Button = {
   func = function() print("Hello World!") end,
 }
 
-function drawButton()
+function createButton(name, x, y, width, height, text, func)
+  buttons[name] = Button:new()
+  buttons[name].x = x
+  buttons[name].y = y
+  buttons[name].text = text
+  buttons[name].func = func
+  buttons[name].width = width
+  buttons[name].height = height
+end
+
+function drawButtons()
   for _, button in pairs(buttons) do
-    local textX = button.x + (button.width - string.len(button.text / 2))
-    local textY = button.y + (button.height / 2)
+    local font = graphics.getFont()
+    local textX = button.x + ((button.width / 2) - (font:getWidth(button.text)/2))
+    local textY = button.y + ((button.height / 2) - (font:getHeight(button.text)/2))
     
     graphics.setColor(button.colour)
-    graphics.rectangle("Fill",, button.y, button.width, button.height)
+    graphics.rectangle("fill", button.x, button.y, button.width, button.height)
     graphics.setColor(0, 0, 0)
-    graphics.print(button.text, textX, textY, button.colour)
+    graphics.print(button.text, textX, textY)
     graphics.setColor(255, 255, 255)
   end
 end
 
 function updtButton()
+  local mouseX, mouseY = love.mouse.getPosition()
+  local mouse = {x = mouseX, y = mouseY, width = 1, height = 1}
   
+  for _, button in pairs(buttons) do
+    if isColliding(mouse, button) then
+      button.colour[4] = 128
+    else
+      button.colour[4] = 255 
+    end
+  end
 end
